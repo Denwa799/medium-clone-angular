a1df894b1746df140246bbe7c87d237f0904ea70
Denwa799 <denwa799@yandex.ru>
2022-10-25 15:53:14 +0300

    Подключает effects

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -2,11 +2,13 @@ import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {ReactiveFormsModule} from '@angular/forms'
 import {RouterModule, Routes} from '@angular/router'
+import { EffectsModule } from '@ngrx/effects'
 import {StoreModule} from '@ngrx/store'
 
 import {RegisterComponent} from 'src/app/auth/components/register/register.component'
 import {reducers} from 'src/app/auth/store/reducers'
-import {AuthService} from './services/auth.service'
+import {AuthService} from 'src/app/auth/services/auth.service'
+import { RegisterEffect } from 'src/app/auth/store/effects/register.effect'
 
 const routes: Routes = [
   {
@@ -21,6 +23,7 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ReactiveFormsModule,
     StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature([RegisterEffect])
   ],
   declarations: [RegisterComponent],
   providers: [AuthService],
