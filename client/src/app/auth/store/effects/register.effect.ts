b1bf94c5079d31f1c5473a815c68d6c238ea43a9
Denwa799 <denwa799@yandex.ru>
2022-11-29 12:24:22 +0300

    Добавляет effect регистрации

--- client/src/app/auth/store/effects/register.effect.ts
+++ client/src/app/auth/store/effects/register.effect.ts
@@ -1,14 +1,16 @@
 import {HttpErrorResponse} from '@angular/common/http'
 import {Injectable} from '@angular/core'
 import {Actions, createEffect, ofType} from '@ngrx/effects'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
+import {PersistanceService} from 'src/app/shared/services/persistance.service'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
-import {AuthService} from '../../services/auth.service'
+import {AuthService} from 'src/app/auth/services/auth.service'
 import {
   registerAction,
   registerFailureAction,
   registerSuccessAction,
-} from '../actions/register.action'
+} from 'src/app/auth/store/actions/register.action'
+import {Router} from '@angular/router'
 
 @Injectable()
 export class RegisterEffect {
@@ -18,6 +20,7 @@ export class RegisterEffect {
       switchMap(({request}) => {
         return this.authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            this.persistanceService.set('accessToken', currentUser.token)
             return registerSuccessAction({currentUser})
           }),
           catchError((errorResponse: HttpErrorResponse) => {
@@ -30,5 +33,21 @@ export class RegisterEffect {
     )
   )
 
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/')
+        })
+      ),
+    {dispatch: false}
+  )
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistanceService: PersistanceService,
+    private router: Router
+  ) {}
 }
