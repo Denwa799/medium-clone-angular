421266163ce0b15ca93013d56e18cce51267f76a
Denwa799 <denwa799@yandex.ru>
2023-02-28 13:28:33 +0300

    Добавляет store для модуля feed

--- client/src/app/shared/modules/feed/feed.module.ts
+++ client/src/app/shared/modules/feed/feed.module.ts
@@ -1,10 +1,20 @@
-import {CommonModule} from '@angular/common'
+import {FeedService} from './services/feed.service'
+import {StoreModule} from '@ngrx/store'
 import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {EffectsModule} from '@ngrx/effects'
+import {GetFeedEffect} from './store/effects/getFeed.effect'
 import {FeedComponent} from './components/globalFeed/feed.component'
+import {reducers} from './store/reducers'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
+  ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
+  providers: [FeedService],
 })
 export class FeedModule {}
