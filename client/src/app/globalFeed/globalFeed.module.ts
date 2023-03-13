b419c1a34d57490a30748ad25f40aa6e53368870
Denwa799 <denwa799@yandex.ru>
2023-03-13 14:54:18 +0300

    Выносит вывод ошибки и сообщения о загрузке в отдельные компоненты

--- client/src/app/globalFeed/globalFeed.module.ts
+++ client/src/app/globalFeed/globalFeed.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {FeedModule} from 'src/app/shared/modules/feed/feed.module'
+import {BannerModule} from 'src/app/shared/modules/banner/banner.module'
 import {GlobalFeedComponent} from './components/globalFeed/globalFeed.component'
 
 const routes: Routes = [
@@ -12,7 +13,12 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), FeedModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FeedModule,
+    BannerModule,
+  ],
   declarations: [GlobalFeedComponent],
 })
 export class GlobalFeedModule {}
--- client/src/app/shared/modules/feed/feed.module.ts
+++ client/src/app/shared/modules/feed/feed.module.ts
@@ -1,12 +1,14 @@
-import {FeedService} from './services/feed.service'
-import {StoreModule} from '@ngrx/store'
 import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {StoreModule} from '@ngrx/store'
 import {CommonModule} from '@angular/common'
 import {EffectsModule} from '@ngrx/effects'
+import {ErrorMessageModule} from 'src/app/shared/modules/errorMessage/errorMessage.module'
+import {LoadingModule} from 'src/app/shared/modules/loading/loading.module'
 import {GetFeedEffect} from './store/effects/getFeed.effect'
 import {FeedComponent} from './components/globalFeed/feed.component'
 import {reducers} from './store/reducers'
-import {RouterModule} from '@angular/router'
+import {FeedService} from './services/feed.service'
 
 @NgModule({
   imports: [
@@ -14,6 +16,8 @@ import {RouterModule} from '@angular/router'
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
     RouterModule,
+    ErrorMessageModule,
+    LoadingModule,
   ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
